408ec279df584ab085ddd76abcba53edf674e0c1
User <abdulhafzi77@gmail.com>
2022-10-13 22:01:52 +0500

    fix smoothbar

--- app/src/main/java/uz/eloving/plantdiseasedetection/Categorization.kt
+++ app/src/main/java/uz/eloving/plantdiseasedetection/Categorization.kt
@@ -1,4 +1,121 @@
 package uz.eloving.plantdiseasedetection
 
-class Categorization {
+import android.content.res.AssetManager
+import android.graphics.Bitmap
+import org.tensorflow.lite.Interpreter
+import java.io.FileInputStream
+import java.nio.ByteBuffer
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import java.util.PriorityQueue
+
+class Categorization(
+    assetManager: AssetManager,
+    modelPath: String,
+    labelPath: String,
+    inputSize: Int,
+) {
+    private val GVN_INP_SZ: Int = inputSize
+    private val PHOTO_SDEVIATE = 225.0f
+    private val GREAT_OUTCOME_MXX = 3
+    private val PITNR: Interpreter
+    private val ROW_LINE: List<String>
+    private val IMAGE_PXL_SZ: Int = 3
+    private val PHOTO_MEN = 0
+    private val POINT_THRHOLD = 0.4f
+
+
+    data class Categorization(
+        var id: String = "",
+        var title: String = "",
+        var confidence: Float = 0F,
+
+        ) {
+
+        override fun toString(): String {
+            return "Title = $title,Confidence=$confidence"
+
+        }
+    }
+
+    init {
+        PITNR = Interpreter(loadModelFile(assetManager, modelPath))
+        ROW_LINE = loadlableList(assetManager, labelPath)
+
+
+    }
+
+    private fun loadlableList(assetManager: AssetManager, labelPath: String): List<String> {
+
+
+        return assetManager.open(labelPath).bufferedReader().useLines { it.toList() }
+
+
+    }
+
+    private fun loadModelFile(assetManager: AssetManager, modelPath: String): MappedByteBuffer {
+
+        val fileDescriptor = assetManager.openFd(modelPath)
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel = inputStream.channel
+        val startOffset = fileDescriptor.startOffset
+        val declaredLength = fileDescriptor.declaredLength
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
+
+    }
+
+    fun recognizeImages(bitmap: Bitmap): List<Categorization> {
+        val scaledBitmap = Bitmap.createScaledBitmap(bitmap, GVN_INP_SZ, GVN_INP_SZ, false)
+        val byteBuffer = convertBitmapToByteBuffer(scaledBitmap)
+        val result = Array(1) { FloatArray(ROW_LINE.size) }
+        PITNR.run(byteBuffer, result)
+        return getSortedResult(result)
+    }
+
+    private fun getSortedResult(result: Array<FloatArray>): List<uz.eloving.plantdiseasedetection.Categorization.Categorization> {
+        val pq = PriorityQueue(GREAT_OUTCOME_MXX,
+            kotlin.Comparator<uz.eloving.plantdiseasedetection.Categorization.Categorization> {
+                (_,_,confidence1),(_,_,confidence2)->java.lang.Float.compare(confidence1,confidence2)*-1
+
+            }
+        )
+        for(i in ROW_LINE.indices){
+           val confidence=result[0][1]
+            if (confidence>=POINT_THRHOLD){
+                pq.add(uz.eloving.plantdiseasedetection.Categorization.Categorization(
+                    ""+i,
+               if (ROW_LINE.size>1)ROW_LINE[i] else "Unknown",confidence
+                ))
+
+            }
+        }
+        val recognitions=ArrayList<uz.eloving.plantdiseasedetection.Categorization.Categorization>()
+        val recognitionsSize=Math.min(pq.size,GREAT_OUTCOME_MXX)
+          for(i in 0 until  recognitionsSize){
+              recognitions.add(pq.poll())
+          }
+
+        return recognitions
+    }
+
+    private fun convertBitmapToByteBuffer(scaledBitmap: Bitmap?): ByteBuffer {
+        val byteBuffer = ByteBuffer.allocateDirect(4 * GVN_INP_SZ * GVN_INP_SZ)
+        val intValue = IntArray(GVN_INP_SZ * GVN_INP_SZ)
+
+        scaledBitmap!!.getPixels(intValue, 0, scaledBitmap.width, 0, 0,scaledBitmap.width, scaledBitmap.height)
+        var pixel = 0
+        for (i in 0 until GVN_INP_SZ) {
+            for (j in 0 until GVN_INP_SZ) {
+                val `val` = intValue[pixel++]
+
+                byteBuffer.putFloat((((`val`.shr(16) and 0xFF) / PHOTO_MEN) / PHOTO_SDEVIATE))
+                byteBuffer.putFloat((((`val`.shr(8) and 0xFF) / PHOTO_MEN) / PHOTO_SDEVIATE))
+                byteBuffer.putFloat((((`val` and 0xFF) / PHOTO_MEN) / PHOTO_SDEVIATE))
+            }
+        }
+        return byteBuffer
+
+    }
+
+
 }
--- app/src/main/java/uz/eloving/plantdiseasedetection/MainActivity.kt
+++ app/src/main/java/uz/eloving/plantdiseasedetection/MainActivity.kt
@@ -1,11 +1,70 @@
 package uz.eloving.plantdiseasedetection
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mCategorization: Categorization
+    private lateinit var mBitmap: Bitmap
+    private val mCameraRequestCode = 0
+    private val mGalleryRequestCode = 2
+    private val inputSize = 224
+    private val mModelPath = "plant_disease_model.tfLite"
+    private val mLabelPath = "plant_labels.txt"
+
+    private val mSamplePath = "automn.jpg"
+    lateinit var toolbar: Toolbar
+    lateinit var drawer: DrawerLayout
+    lateinit var navigationView: NavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigationView = findViewById<View>(R.id.nav_views) as NavigationView
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        drawer.setViewScale(GravityCompat.START, 0.9f)
+
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, 0, 0
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        navigationView.setNavigationItemSelectedListener(this)
+        drawer.setViewElevation(GravityCompat.START, Color.TRANSPARENT)
+        drawer.drawerElevation(GravityCompat.START, 20f)
+        drawer.setContrastThreshold(3f)
+        drawer.setRadious(GravityCompat.START, 25f)
+        drawer.useCustomBehaviour(GravityCompat.START)
+        drawer.useCustomBehaviour(GravityCompat.END)
+        val headerview = navigationView.getHeaderView(0)
+
+        mCategorization = Categorization(assets, mModelPath, mLabelPath, inputSize)
+
+
+        resources.assets.open(mSamplePath).use {
+            mBitmap=BitmapFactory.decodeStream(it)
+            mBitmap=Bitmap.createScaledBitmap(mBitmap,inputSize,inputSize,TR)
+         mPhotoImageView.setImageBitmap(mBitmap)
+
+
+
+        }
+val bottomNav:SmoothBottombar=findViewById<SmoothBottomBar>(R.id.bottom_nav)
+
     }
 }
